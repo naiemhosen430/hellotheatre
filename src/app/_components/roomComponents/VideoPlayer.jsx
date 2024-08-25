27ef34642035a0bdf8c49d0c77e9c667d06d0785
naiemhosen430 <naiemhosen430@gmail.com>
2024-08-25 23:59:51 +0600

    d

--- src/app/_components/roomComponents/VideoPlayer.jsx
+++ src/app/_components/roomComponents/VideoPlayer.jsx
@@ -1,24 +1,74 @@
-import Image from "next/image";
-import React from "react";
+"use client";
+import React, { useState, useRef } from "react";
 import { Container } from "react-bootstrap";
 import { RxAvatar } from "react-icons/rx";
 
 export default function VideoPlayer() {
+  const [videoId, setVideoId] = useState("TlC_NCowUuQ");
+  const [errorData, setErrorData] = useState("");
+  const inputRef = useRef(null);
+
+  const handleVideoChange = (url) => {
+    try {
+      const urlObj = new URL(url);
+      // Check if the hostname is YouTube
+      if (!["www.youtube.com", "youtu.be"].includes(urlObj.hostname)) {
+        console.log("Invalid URL. Please enter a valid YouTube URL.");
+        return;
+      }
+
+      const videoId =
+        urlObj.searchParams.get("v") || urlObj.pathname.split("/").pop();
+      setVideoId(videoId);
+      setErrorData("");
+    } catch (error) {
+      setErrorData("Invalid URL. Please enter a valid YouTube URL.");
+    }
+  };
+
+  const handleDefaultVideo = () => {
+    setVideoId("TlC_NCowUuQ");
+    if (inputRef.current) {
+      inputRef.current.value = ""; // Clear the input field
+    }
+  };
+
   return (
     <Container>
       <div className="lg:flex">
         <div className="w-12/12 lg:w-8/12">
           <h1 className="p-2 text-slate-400 bg-slate-800">Movie Theatre</h1>
-          <div className="border-2 p-2 border-x-sky-950">
-            <video
-              id="videoPlayer"
+          <div className="border-2 border-x-sky-950">
+            <iframe
+              id="youtubePlayer"
+              className="lg:max-h-[400px] max-h-[250px]"
               width="100%"
-              controls
-              className="rounded-md"
-            >
-              <source src="your-video-url.mp4" type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
+              height="500"
+              src={`https://www.youtube.com/embed/${videoId}?autoplay=1&rel=1&controls=1`}
+              title="YouTube video player"
+              frameBorder="0"
+              allow="autoplay; encrypted-media"
+              allowFullScreen
+            ></iframe>
+
+            {errorData && (
+              <p className="text-white text-[10px] px-2">{errorData}</p>
+            )}
+            <div className="suggestions px-2 flex items-center">
+              <input
+                type="text"
+                placeholder="Enter YouTube URL"
+                ref={inputRef}
+                className="w-8/12 p-2 m-1 lg:text-sm text-[10px] rounded-md bg-slate-700 text-white"
+                onChange={(e) => handleVideoChange(e.target.value)}
+              />
+              <button
+                className="bg-slate-700 w-4/12 text-white px-2 py-2 m-1 rounded-xl lg:text-sm text-[10px]"
+                onClick={handleDefaultVideo}
+              >
+                Play Default Video
+              </button>
+            </div>
           </div>
         </div>
 
@@ -26,7 +76,7 @@ export default function VideoPlayer() {
           <h1 className="p-2 text-slate-400 bg-slate-800">People (12)</h1>
           <div className="p-4 px-2">
             <div className="flex">
-              <div className="flex p-2 text-white justify-center ">
+              <div className="flex p-2 text-white justify-center">
                 <RxAvatar className="text-5xl" />
               </div>
               <div className="p-2">
