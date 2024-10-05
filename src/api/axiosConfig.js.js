ffbed2e124948e55375cc7fba4cc73878226dd1f
MD Naiem Hosen <109727997+naiemhosen430@users.noreply.github.com>
2024-10-05 23:34:29 +0600

    Update axiosConfig.js.js

--- src/api/axiosConfig.js.js
+++ src/api/axiosConfig.js.js
@@ -9,8 +9,8 @@ const apiBaseURL = process.env.SERVER_URL;
 
 // Create Axios instance
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
-  // baseURL: "https://api-hellotheatre.onrender.com",
+  // baseURL: "http://localhost:5000",
+  baseURL: "https://api-hellotheatre.onrender.com",
   headers: {
     "Content-Type": "application/json",
     ...(token && { Authorization: `Bearer ${token}` }),
